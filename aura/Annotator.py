1001f79b5b3052fe8a10272885f0e1eb4eb0bee1
Zeio Nara <zeionara@gmail.com>
2025-11-14 23:05:31 +0300

    fix(table-label-lookup): updated table lookup algorithm

--- aura/Annotator.py
+++ aura/Annotator.py
@@ -2,11 +2,12 @@ import re
 from os import path as os_path, mkdir, walk
 from tqdm import tqdm
 
-from logging import getLogger, INFO, ERROR
+from logging import getLogger, INFO, ERROR, WARNING
 from time import time
+from string import punctuation
 
 from .VllmClient import VllmClient
-from .util import make_system_prompt, make_annotation_prompt, read_elements, dict_to_string, string_to_dict, dict_to_json_file, get_xml, dict_to_json_file
+from .util import make_system_prompt, make_annotation_prompt, read_elements, dict_to_string, string_to_dict, dict_to_json_file, get_xml, dict_to_json_file, normalize_spaces
 from .document import Paragraph, Cell, Table
 from .document.ZipFile import ZipFile
 
@@ -17,6 +18,9 @@ logger.setLevel(INFO)
 
 TABLE_TITLE_PATTERN = re.compile(r'(?:таблица|форма|приложение)\s+([^ ]+)')
 EXCEPTIONAL_TABLE_LABELS = frozenset({'библиография'})
+FORBIDDEN_TABLE_LABELS = frozenset({'технического', 'справкио'})
+
+PUNCTUATION_TRANSLATOR = str.maketrans('', '', punctuation)
 
 
 def generate_batches(items: list[int], n: int):
@@ -25,15 +29,70 @@ def generate_batches(items: list[int], n: int):
     return [items[i: i + n] for i in range(0, len(items), n)]
 
 
+def remove_punctuation(input_string):
+    return input_string.translate(PUNCTUATION_TRANSLATOR)
+
+
+def is_part_of_form(table_label_candidates: list[str]):
+    for candidate in table_label_candidates:
+        if len(candidate) < 1:
+            continue
+
+        unique_chars = set(candidate)
+
+        if len(unique_chars) < 2 and unique_chars.pop() == '_':
+            return True
+
+    return False
+
+
+def make_table_header(table_label_candidates: list[str]):
+    label_length = 0
+    label = None
+
+    for candidate in table_label_candidates:
+        parts = normalize_spaces(candidate).split(' ')
+
+        if len(parts) > 4:
+            label_length = 4
+            label = remove_punctuation(f'{parts[1]} {parts[2]} {parts[3]} {parts[4]}')
+            continue
+
+        if len(parts) > 3 and label_length < 3:
+            label_length = 3
+            label = remove_punctuation(f'{parts[1]} {parts[2]} {parts[3]}')
+            continue
+
+        if len(parts) > 2 and label_length < 2:
+            label_length = 2
+            label = remove_punctuation(f'{parts[1]} {parts[2]}')
+            continue
+
+        if len(parts) > 1 and label_length <= 1:
+            label_length = 1
+            label = remove_punctuation(parts[1])
+            continue
+
+        if len(parts) > 0 and label_length < 1:
+            label_length = 1
+            label = remove_punctuation(parts[0])
+            continue
+
+    return label
+
+
 class Annotator:
     def __init__(self, llms: list[VllmClient]):
         self.llms = llms
 
-    def annotate(self, input_path: str, output_path: str, batch_size: int = None, n_batches: int = None, table_label_search_window: int = 10, dry_run: bool = False):
+    def annotate(self, input_path: str, output_path: str, batch_size: int = None, n_batches: int = None, table_label_search_window: int = 20, dry_run: bool = False):
         if not os_path.isdir(output_path):
             mkdir(output_path)
 
         llms = self.llms
+
+        n_tables = 0
+        n_paragraphs = 0
 
         for root, _, files in walk(input_path):
             for file in files:
@@ -45,11 +104,16 @@ class Annotator:
 
                 annotations = {}
                 offset = 0
+                form_count = 0
+                missing_label_count = 0
 
                 table_label_candidates = []
+                seen_labels = set()
 
                 elements = read_elements(os_path.join(root, file))
+
                 output_file = file[:-5] + '.json'
+                output_filename = os_path.join(output_path, output_file)
 
                 for element in elements:
                     if element.tag.endswith('}p'):
@@ -58,7 +122,12 @@ class Annotator:
                         if paragraph:
                             paragraphs.append(paragraph)
                             table_label_candidates.append(paragraph.text)
+
+                            n_paragraphs += 1
                     else:
+                        if len(paragraphs) < 1:
+                            continue
+
                         i = 0
                         label = None
 
@@ -94,12 +163,22 @@ class Annotator:
                             if match is None:
                                 if title in EXCEPTIONAL_TABLE_LABELS:
                                     label = title
-                            else:
+                            elif match.group(1) not in FORBIDDEN_TABLE_LABELS:
                                 label = match.group(1)
+
+                                if 'форма' in title:
+                                    label = f'форма {label}'
+
+                                if 'приложение' in title:
+                                    label = f'приложение {label}'
+
                                 break
 
                         if label is None:  # Try to find the table header in the table itself
                             for cell in table.cells:
+                                if cell is None:
+                                    continue
+
                                 title = cell.lower()
                                 match = TABLE_TITLE_PATTERN.match(title)
 
@@ -107,31 +186,55 @@ class Annotator:
                                     label = match.group(1)
                                     break
 
+                        if is_part_of_form(seen_candidates):
+                            form_count += 1
+                            label = f'часть формы {form_count}'
+
+                        if label is None:  # Try to generate table name from seen candidates
+                            label = make_table_header(seen_candidates)
+                            logger.warning('%s - Had to generate table name "%s"', file, label)
+
                         if label is None:
-                            logger.error(
-                                '%s - Can\'t find table label (table size = %d x %d, total paragraphs count = %d, total tables count = %d) among candidates: %s',
-                                file,
-                                table.n_rows,
-                                table.n_cols,
-                                len(paragraphs),
-                                len(tables),
-                                ", ".join(f'"{candidate}"' for candidate in seen_candidates)
-                            )
-                        else:
-                            annotations[label] = {
-                                'type': 'table',
-                                'paragraphs': []
-                            }
-                            table.label = label
-                            logger.info('%s - Found table %s', file, label)
+                            missing_label_count += 1
+                            label = f'таблица без названия {missing_label_count}'
+
+                        # if label is None:
+                        #     logger.error(
+                        #         '%s - Can\'t find table label (table size = %d x %d, total paragraphs count = %d, total tables count = %d) among candidates: %s',
+                        #         file,
+                        #         table.n_rows,
+                        #         table.n_cols,
+                        #         len(paragraphs),
+                        #         len(tables),
+                        #         ", ".join(f'"{candidate}"' for candidate in seen_candidates)
+                        #     )
+                        # else:
+
+                        label = label.replace('(справочное)', '')
+                        label = label.replace('(рекомендуемое)', '')
+
+                        while label in seen_labels:
+                            label = f'{label}_'
+                            # logger.warning('%s - Duplicate label %s', file, label)
+
+                        seen_labels.add(label)
+
+                        annotations[label] = {
+                            'type': 'table',
+                            'paragraphs': []
+                        }
+                        table.label = label
+                        logger.info('%s - Found table %s', file, label)
 
                         table_label_candidates = []
 
                         tables.append(table)
 
+                        n_tables += 1
+
                 logger.info('%s - Found %d tables and %d paragraphs', file, len(tables), len(paragraphs))
 
-                if dry_run:
+                if dry_run or os_path.isfile(output_filename):
                     continue
 
                 batched_paragraphs = generate_batches(paragraphs, batch_size)
@@ -206,5 +309,7 @@ class Annotator:
 
                 dict_to_json_file(
                     annotations,
-                    os_path.join(output_path, output_file)
+                    output_filename
                 )
+
+        logger.info('Total - Found %d tables and %d paragraphs', n_tables, n_paragraphs)
